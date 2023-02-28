aec384de2e3bd4642fcd6a99cc1c008799304910
Rikin Marfatia <rikinm10@gmail.com>
2023-02-28 09:33:09 -0800

    Built a new squiggle slider user @saketme's technique

--- app/src/main/java/co/rikin/squiggles/MainActivity.kt
+++ app/src/main/java/co/rikin/squiggles/MainActivity.kt
@@ -1,9 +1,11 @@
 package co.rikin.squiggles
 
 import android.graphics.Paint
+import android.graphics.PathMeasure
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.InfiniteRepeatableSpec
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
@@ -22,6 +24,7 @@ import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -37,6 +40,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.BiasAlignment
@@ -59,17 +63,18 @@ import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.input.pointer.changedToUp
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.rikin.squiggles.ui.theme.Pink80
-import co.rikin.squiggles.ui.theme.Purple40
 import co.rikin.squiggles.ui.theme.Purple80
 import co.rikin.squiggles.ui.theme.PurpleGrey40
 import co.rikin.squiggles.ui.theme.PurpleGrey80
 import co.rikin.squiggles.ui.theme.SquigglesTheme
+import kotlinx.coroutines.launch
 import kotlin.math.ceil
 import kotlin.math.roundToInt
 import kotlin.math.sin
@@ -440,23 +445,21 @@ fun Int.pi() = this * PI
 //  }
 //}
 //
-@Preview
 @Composable
-fun NewSquiggleSlider() {
-  var percent by remember { mutableStateOf(0.5f) }
+fun NewSquiggleSlider(modifier: Modifier = Modifier, state: Float, update: (Float) -> Unit) {
+  var percent by remember { mutableStateOf(state) }
 
   val infiniteTransition = rememberInfiniteTransition()
   val wave by infiniteTransition.animateFloat(
     initialValue = 0f,
     targetValue = 1f,
     animationSpec = infiniteRepeatable(
-      animation = tween(1000, easing = LinearEasing),
+      animation = tween(1_500, easing = LinearEasing),
       repeatMode = RepeatMode.Restart
     )
   )
   Box(
-    modifier = Modifier
-      .fillMaxWidth()
+    modifier = modifier
       .height(35.dp)
       .background(color = PurpleGrey40)
       .pointerInput(Unit) {
@@ -471,13 +474,14 @@ fun NewSquiggleSlider() {
               )
               val normalizedX = x / size.width
               percent = normalizedX
+              update(normalizedX)
             } while (event.changes.none { it.changedToUp() })
           }
         }
       }
       .drawBehind {
         val padding = 16.dp.toPx()
-        val wavelength = 32.dp.toPx()
+        val wavelength = 48.dp.toPx()
         val amplitude = 4.dp.toPx()
         val yShift = size.height / 2
 
@@ -532,13 +536,34 @@ fun NewSquiggleSlider() {
 @Preview
 @Composable
 fun NewSquiggleSliderPreview() {
-  Box(
-    modifier = Modifier
-      .fillMaxSize()
-      .background(color = PurpleGrey40),
-    contentAlignment = Alignment.Center
-  ) {
-    NewSquiggleSlider()
+  SquigglesTheme {
+    Box(
+      modifier = Modifier
+        .fillMaxSize()
+        .background(color = PurpleGrey40)
+        .padding(16.dp),
+      contentAlignment = Alignment.Center
+    ) {
+      val low = 0
+      val high = 128
+      var percent by remember { mutableStateOf(0f) }
+      Row(
+        verticalAlignment = Alignment.CenterVertically
+      ) {
+        NewSquiggleSlider(
+          modifier = Modifier.weight(.80f),
+          state = percent,
+          update = { percent = it }
+        )
+        Text(
+          modifier = Modifier.weight(.20f),
+          text = "${(percent * (high - low)).toInt()} oz",
+          color = Purple80,
+          fontSize = 16.sp,
+          textAlign = TextAlign.Center
+        )
+      }
+    }
   }
 }
 
